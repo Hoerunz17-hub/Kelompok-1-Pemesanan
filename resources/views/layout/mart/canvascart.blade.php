93313fdacbf6e3352df2125607a087126aeffc49
akuiniwinda <windanurazizh@gmail.com>
2025-11-15 14:44:59 +0700

    fix bagian order

--- resources/views/layout/mart/canvascart.blade.php
+++ resources/views/layout/mart/canvascart.blade.php
@@ -5,8 +5,14 @@
       <div class="offcanvas-body">
         <div class="order-md-last">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-primary">Your cart</span>
-            <span class="badge bg-primary rounded-pill">3</span>
+            <span class="text-primary">Order</span>
+            <span class="badge bg-primary rounded-pill">
+                <select class="transparent-arrows">
+                    <option>001</option>
+                    <option>002</option>
+                    <option>003</option>
+                </select>
+            </span>
           </h4>
           <ul class="list-group mb-3">
             <li class="list-group-item d-flex justify-content-between lh-sm">
@@ -16,44 +22,39 @@
               </div>
               <span class="text-body-secondary">$12</span>
             </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Fresh grapes</h6>
-                <small class="text-body-secondary">Brief description</small>
-              </div>
-              <span class="text-body-secondary">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Heinz tomato ketchup</h6>
-                <small class="text-body-secondary">Brief description</small>
-              </div>
-              <span class="text-body-secondary">$5</span>
-            </li>
             <li class="list-group-item d-flex justify-content-between">
               <span>Total (USD)</span>
               <strong>$20</strong>
             </li>
           </ul>
 
-            <a class="w-100 btn btn-primary btn-lg" href="/payment">Continue to checkout</a>
+            <div class="d-flex gap-2 mb-3">
+                <label class="form-label text-body-primary">Name</label>
+                <input type="text" class="form-control" name="" >
+                <label class="form-label">No Invoice</label>
+                <input type="text" class="form-control" name="" >
+            </div>
+
+            <div class="d-flex gap-2 mb-3">
+                <select class="form-select transparent-arrow" style="background-color:#ffe19f;">
+                    <option>Dine in</option>
+                    <option>Take Away</option>
+                </select>
+
+                <select class="form-select transparent-arrow" style="background-color:#ffe19f;">
+                    <option>Yanto</option>
+                    <option>Yanti</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Note</label>
+                <input type="text" class="form-control" name="" style="background-color:#ffe19f;">
+            </div>
+
+            <a class="w-100 btn btn-primary btn-lg" href="/payment">Konfirmasi Pesanan</a>
         </div>
       </div>
     </div>
 
-    <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasSearch" aria-labelledby="Search">
-      <div class="offcanvas-header justify-content-center">
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <div class="order-md-last">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-primary">Search</span>
-          </h4>
-          <form role="search" action="index.html" method="get" class="d-flex mt-3 gap-0">
-            <input class="form-control rounded-start rounded-0 bg-light" type="email" placeholder="What are you looking for?" aria-label="What are you looking for?">
-            <button class="btn btn-dark rounded-end rounded-0" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </div>
+
