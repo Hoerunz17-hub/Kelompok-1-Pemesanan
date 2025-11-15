251966fd29e6b0bd31b74718708925fc1789e147
Hoerunz17-hub <khoerunmaulana1@gmail.com>
2025-11-15 12:07:14 +0700

    perbaikan 2

--- resources/views/layout/mart/app.blade.php
+++ resources/views/layout/mart/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <title>FoodMart - Free eCommerce Grocery Store HTML Website Template</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,36 +13,43 @@
     <meta name="description" content="">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('assetsfrontend/css/vendor.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assetsfrontend/style.css') }}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    @include('layout.frontend.mart.css')
-  </head>
-  <body>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet">
+    @include('layout.mart.css')
+</head>
 
-    @include('layout.frontend.mart.svg')
+<body>
+
+    @include('layout.mart.svg')
 
     <div class="preloader-wrapper">
-      <div class="preloader">
-      </div>
+        <div class="preloader">
+        </div>
     </div>
 
-    @include('layout.frontend.mart.canvascart')
+    @include('layout.mart.canvascart')
 
-    @include('layout.frontend.mart.navbar')
+    @include('layout.mart.navbar')
 
     @yield('content')
 
-    @include('layout.frontend.mart.footer')
+    @include('layout.mart.footer')
 
     <script src="{{ asset('assetsfrontend/js/jquery-1.11.0.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
     <script src="{{ asset('assetsfrontend/js/plugins.js') }}"></script>
     <script src="{{ asset('assetsfrontend/js/script.js') }}"></script>
-  </body>
+</body>
+
 </html>
