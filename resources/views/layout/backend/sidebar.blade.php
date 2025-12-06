4d2b0fdfdbbcc8ca46e09de30562b14b0a403ab0
Amat29-hub <matz777777@gmail.com>
2025-12-06 10:33:35 +0700

    feat: menerapkan tampilan sidebar berdasarkan role super_admin dan admin

--- resources/views/layout/backend/sidebar.blade.php
+++ resources/views/layout/backend/sidebar.blade.php
@@ -4,36 +4,67 @@
             <h5 class="logo-text">SEJAHTERA ADMIN</h5>
         </a>
     </div>
+
     <ul class="sidebar-menu do-nicescrol">
         <li class="sidebar-header">MAIN NAVIGATION</li>
-        <li class="{{ Request::is('adminpanel*') ? 'active' : '' }}">
-            <a href="/adminpanel">
-                <i class="zmdi zmdi-view-dashboard"></i> <span>Dashboard</span>
-            </a>
-        </li>
 
-        <li class="{{ Request::is('user*') ? 'active' : '' }}">
-            <a href="/user">
-                <i class="zmdi zmdi-invert-colors"></i> <span>User</span>
-            </a>
-        </li>
+        {{-- ================= SUPER ADMIN (SEMUA MENU MUNCUL) ================= --}}
+        @if(Auth::user()->role == 'super_admin')
 
-        <li class="{{ Request::is('product*') ? 'active' : '' }}">
-            <a href="/product">
-                <i class="zmdi zmdi-format-list-bulleted"></i> <span>Product</span>
-            </a>
-        </li>
-        <li class="{{ Request::is('order*') ? 'active' : '' }}">
-            <a href="/order">
-                <i class="zmdi zmdi-format-list-bulleted"></i> <span>Order</span>
-            </a>
-        </li>
-        <li class="sidebar-header">LAPORAN </li>
-        <li class="{{ Request::is('report/today') ? 'active' : '' }}">
-            <a href="/report/today">
-                <i class="zmdi zmdi-info-outline text-info"></i>
-                <span>Information</span>
-            </a>
-        </li>
+            <li class="{{ Request::is('adminpanel*') ? 'active' : '' }}">
+                <a href="/adminpanel">
+                    <i class="zmdi zmdi-view-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="{{ Request::is('user*') ? 'active' : '' }}">
+                <a href="/user">
+                    <i class="zmdi zmdi-accounts"></i>
+                    <span>User</span>
+                </a>
+            </li>
+
+            <li class="{{ Request::is('product*') ? 'active' : '' }}">
+                <a href="/product">
+                    <i class="zmdi zmdi-format-list-bulleted"></i>
+                    <span>Product</span>
+                </a>
+            </li>
+
+            <li class="{{ Request::is('order*') ? 'active' : '' }}">
+                <a href="/order">
+                    <i class="zmdi zmdi-shopping-cart"></i>
+                    <span>Order</span>
+                </a>
+            </li>
+
+            <li class="sidebar-header">LAPORAN</li>
+
+            <li class="{{ Request::is('report/today') ? 'active' : '' }}">
+                <a href="/report/today">
+                    <i class="zmdi zmdi-info-outline text-info"></i>
+                    <span>Information</span>
+                </a>
+            </li>
+
+        {{-- ================= ADMIN (Dashboard + Order saja) ================= --}}
+        @elseif(Auth::user()->role == 'admin')
+
+            <li class="{{ Request::is('adminpanel*') ? 'active' : '' }}">
+                <a href="/adminpanel">
+                    <i class="zmdi zmdi-view-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="{{ Request::is('order*') ? 'active' : '' }}">
+                <a href="/order">
+                    <i class="zmdi zmdi-shopping-cart"></i>
+                    <span>Order</span>
+                </a>
+            </li>
+
+        @endif
     </ul>
 </div>
